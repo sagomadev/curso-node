b471fda16a40b08e75c4c2369c696bfe68e4fdb0
Samuel Gómez <sagomadev@gmail.com>
2023-10-08 14:50:31 +0200

    00002 - Modificar y guardar en un nuevo archivo

--- 01-Fundamentos/app2.js
+++ 01-Fundamentos/app2.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 
+// Leemos el archivo readme.md
 const data = fs.readFileSync("readme.md", "utf-8");
 
-console.log(data);
+// Reemplazamos la palabra React por Angular
+const newData = data.replace(/React/gi, "Angular");
+
+// Escribimos el archivo readme-angular.md
+fs.writeFileSync("readme-angular.md", newData);
